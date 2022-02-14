54abde1fc5c1a06d8f8da1326cf5225d25c5e67c
AkshaySharma008 <akshaysharma30121999@gmail.com>
2022-02-14 21:24:52 +0530

    added frontend build

---
+++ public/frontend/precache-manifest.f1302e51411d36e2b798998133c0632f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65e2834bbbdd5e6c94d10b9238adb72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2e7a25e1203ca4a2a02",
+    "url": "/static/css/2.b8803beb.chunk.css"
+  },
+  {
+    "revision": "9503a37327a4b426696d",
+    "url": "/static/css/main.0d7b0667.chunk.css"
+  },
+  {
+    "revision": "c2e7a25e1203ca4a2a02",
+    "url": "/static/js/2.ebc55702.chunk.js"
+  },
+  {
+    "revision": "2f0da1418a85a0909085c08f49a1ad9b",
+    "url": "/static/js/2.ebc55702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9503a37327a4b426696d",
+    "url": "/static/js/main.fdf89e0a.chunk.js"
+  },
+  {
+    "revision": "432c4d19aea018b3ec6d",
+    "url": "/static/js/runtime-main.a7f0ecfc.js"
+  },
+  {
+    "revision": "9960d83ea7f276f61fa41cc87c07772b",
+    "url": "/static/media/account_circle-24px.9960d83e.svg"
+  },
+  {
+    "revision": "ac4cea06e3682e029afc3856180a7941",
+    "url": "/static/media/business-24px.ac4cea06.svg"
+  },
+  {
+    "revision": "ba66d505714a3f97ce9d552f65708d95",
+    "url": "/static/media/email-24px.ba66d505.svg"
+  },
+  {
+    "revision": "50538e10603eaa930182af1fbec8978f",
+    "url": "/static/media/iconfinder_160_Hackerrank_logo_logos_4373234.50538e10.png"
+  },
+  {
+    "revision": "558fac1adfd2ae2ad495b947317153c0",
+    "url": "/static/media/iconfinder_2018_social_media_popular_app_logo_linkedin_3225190.558fac1a.png"
+  },
+  {
+    "revision": "f0b5cb1edcddc65723dfff17bf1da677",
+    "url": "/static/media/iconfinder_2018_social_media_popular_app_logo_twitter_3225183.f0b5cb1e.png"
+  },
+  {
+    "revision": "9935f62cabb186c0996c606fbd6f907b",
+    "url": "/static/media/iconfinder_Circled_Medium_svg5_5279113.9935f62c.png"
+  },
+  {
+    "revision": "957c4dd9aa815cc1de2040cc1259589f",
+    "url": "/static/media/iconfinder_github_317712.957c4dd9.png"
+  },
+  {
+    "revision": "98f4f6841f3d482b15c3a9d79c8db50f",
+    "url": "/static/media/insert_link-24px (1).98f4f684.svg"
+  },
+  {
+    "revision": "a9efc0d96889659ea5bc98b7c0b32ed5",
+    "url": "/static/media/location_on-24px.a9efc0d9.svg"
+  },
+  {
+    "revision": "3613163b1d67dc2a311d1e3b4fed22ac",
+    "url": "/static/media/north_east-24px.3613163b.svg"
+  },
+  {
+    "revision": "db5d8122836517965b6e0247a3a9691c",
+    "url": "/static/media/search-24px.db5d8122.svg"
+  },
+  {
+    "revision": "ac56aa6deef5ec60f1a68b9d89764664",
+    "url": "/static/media/undraw_dev_productivity_umsq 1.ac56aa6d.png"
+  }
+]);
